8d537ad8abf3decca63c2383153cc41191223758
dinurajd <120137529+dinurajd@users.noreply.github.com>
2023-01-10 22:17:26 +0800

    Update main.tf

--- 03-basics/aws-backend/main.tf
+++ 03-basics/aws-backend/main.tf
@@ -46,7 +46,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_c
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-state-locking"
+  name         = "terraform-state"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
