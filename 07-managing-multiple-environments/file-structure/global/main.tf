8c14b16e8f90ae5522300191470b15e030119aaa
James Leatherman <24658347+leathej1@users.noreply.github.com>
2024-01-18 08:31:07 -0500

    Add unique bucket id

--- 07-managing-multiple-environments/file-structure/global/main.tf
+++ 07-managing-multiple-environments/file-structure/global/main.tf
@@ -2,7 +2,7 @@ terraform {
   # Assumes s3 bucket and dynamo DB table already set up
   # See /code/03-basics/aws-backend
   backend "s3" {
-    bucket         = "devops-directive-tf-state"
+    bucket         = "devops-directive-tf-state-264369"
     key            = "07-managing-multiple-environments/global/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-locking"
